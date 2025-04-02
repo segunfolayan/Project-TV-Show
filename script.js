359731817b8962c320fcee5e557efe56f2eeaa75
Segun <segunfolayan@yahoo.com>
2025-04-02 23:19:29 +0100

    first addition

--- script.js
+++ script.js
@@ -4,9 +4,37 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+//function makePageForEpisodes(episodeList) {
+ // const rootElem = document.getElementById("root");
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+//}
+
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const template = document.getElementById("episode-template"); 
+
+  rootElem.innerHTML = ""; 
+
+  episodeList.forEach((episode) => {
+    
+    const episodeCard = template.content.firstElementChild.cloneNode(true);
+
+  
+    episodeCard.querySelector("#title").textContent = 
+      `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")} - ${episode.name}`;
+
+    episodeCard.querySelector(".episode-image img").src = 
+      episode.image ? episode.image.medium : "https://via.placeholder.com/210x295";
+    episodeCard.querySelector(".episode-image").alt = episode.name;
+
+    episodeCard.querySelector("#details").innerHTML = episode.summary;
+
+    rootElem.appendChild(episodeCard);
+  });
 }
 
+// Run setup when the page loads
 window.onload = setup;
+
+
