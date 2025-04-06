71f311ce12cb29595a06de6a2bbbb586ea34ade0
Elchacode <elchaschool@gmail.com>
2025-04-06 14:53:57 +0100

    implemented a div element and also a template literal in other to get the episode length

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function makePageForEpisodes(films) {
   const movieContainer = document.querySelector("#movie-container");
   const movieTemplate = document.querySelector("#movie-template");
+  const episodeCount = document.createElement("div"); // createed a div element for each episode count in other to count how many episodes.
+  episodeCount.id = "episode-count"; // the id of the episode count.
+  episodeCount.textContent = `Got ${films.length} episode(s)`; // implemented template literal to display the number of episodes.
 
-  
-  films.forEach(film => {
+  movieContainer.appendChild(episodeCount);
+
+  films.forEach((film) => {
     const movieCard = movieTemplate.content.cloneNode(true);
 
-    movieCard.querySelector(".title").textContent = 
-    
-    `${film.name}: S${String(film.season).padStart(2,"0")}E${String(film.number).padStart(2,"0")}` ;
+    movieCard.querySelector(".title").textContent = `${film.name}: S${String(
+      film.season
+    ).padStart(2, "0")}E${String(film.number).padStart(2, "0")}`;
 
     const img = movieCard.querySelector("img");
     img.src = film.image.medium;
@@ -31,6 +35,3 @@ function setup() {
 }
 
 window.onload = setup;
-
-
-
